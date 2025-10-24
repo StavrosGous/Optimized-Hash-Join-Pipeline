bf65ca7deda41c6f28305aa3fcddcf65544b2625
JohnVogia <johnvogiatzis1967@gmail.com>
2025-10-24 18:50:26 +0300

    some more unit tests

--- tests/HashMap_tests.cpp
+++ tests/HashMap_tests.cpp
@@ -2,6 +2,7 @@
 
 #include "RHMap.h"
 
+// Test case to verify initialization of Robin_Hood_HashMap
 TEST_CASE("Initialization of Robin_Hood_HashMap", "[HashMap]") {
     RHMap<int, std::vector<int>> *map = new RHMap<int, std::vector<int>>(10ll);
     map->emplace(1, std::vector<int>{2, 3});
@@ -10,4 +11,37 @@ TEST_CASE("Initialization of Robin_Hood_HashMap", "[HashMap]") {
     REQUIRE(map->get_count() == 3);
     REQUIRE(map->get_capacity() == 1 << (sizeof(size_t) * 8 - __builtin_clzll(10 - 1)));
     delete map;
+}
+//Test case to check if end() returns casted NULL pointer
+TEST_CASE("End Function of Robin_Hood_HashMap", "[HashMap]") {
+    RHMap<int, std::vector<int>> map(10ll);
+    auto end_ptr = map.end();
+    REQUIRE(end_ptr == NULL);
+}
+
+// Test case to test successful insertion and finding of keys
+TEST_CASE("Insertion and Finding in Robin_Hood_HashMap", "[HashMap]") {
+    RHMap<int, std::vector<int>> map(10ll);
+    map.emplace(1, std::vector<int>{2, 3});
+    map.emplace(2, std::vector<int>{4, 5});
+    map.emplace(3, std::vector<int>{6, 7});
+    //Ensure find returns the right values for all inserted keys
+    for (int i = 1; i <=3; ++i) {
+        auto &itr = map.find(i);
+        REQUIRE(itr != map.end());
+        REQUIRE(itr->second == std::vector<int>{2*i, 2*i+1});
+    }
+}
+
+// Test case to check if end() is returned when searching non-existent keys
+TEST_CASE("Finding Non-Existent Keys in Robin_Hood_HashMap", "[HashMap]") {
+    RHMap<int, std::vector<int>> map(10ll);
+    map.emplace(1, std::vector<int>{2, 3});
+    map.emplace(2, std::vector<int>{4, 5});
+    map.emplace(3, std::vector<int>{6, 7});
+    //Ensure find returns end() for non-existent keys
+    for (int i = 4; i <=6; ++i) {
+        auto &itr = map.find(i);
+        REQUIRE(itr == map.end());
+    }
 }
