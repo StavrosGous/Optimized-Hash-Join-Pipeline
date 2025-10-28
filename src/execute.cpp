506308900e26c976bd81cb2407c37307539f43b9
StavrosGous <gousgounisstavros1@gmail.com>
2025-10-28 22:04:55 +0200

    optimizations to hopscotch similar ot the robin hood. changed boll array of bitmap to uint64_t variable as our H is 64. addedd tricks in bitmap calculatiosn to avoid many loops and added an initial bucket load in each loop so we dont call b[idx] or b[pos], etc in every loop

--- src/execute.cpp
+++ src/execute.cpp
@@ -2,7 +2,6 @@
 #include <plan.h>
 #include <table.h>
 #include "RHMap.h"
-#include "CuckooMap.h"
 #include "HopscotchMap.h"
 namespace Contest {
 
@@ -21,10 +20,7 @@ struct JoinAlgorithm {
     template <class T>
     auto run() {
         namespace views = ranges::views;
-    // std::unordered_map<T, std::vector<size_t>> hash_table;
-    // RHMap<T, std::vector<size_t>> hash_table(build_left ? left.size() * 2 : right.size() * 2);
-    HopscotchMap<T, std::vector<size_t>> hash_table(build_left ? left.size() * 2 : right.size() * 2);
-    // CuckooMap<T, std::vector<size_t>> hash_table(build_left ? left.size() * 2: right.size() * 2);
+        HopscotchMap<T, std::vector<size_t>> hash_table(build_left ? left.size() << 1 : right.size() << 1);
         if (build_left) {
             for (auto&& [idx, record]: left | views::enumerate) {
                 std::visit(
