fba5fa391a788332796647f41f0f85443050f5c6
StavrosGous <gousgounisstavros1@gmail.com>
2025-11-06 20:23:00 +0200

    removed initializaiton test of robin hood as it was needless

--- tests/robinhood_tests.cpp
+++ tests/robinhood_tests.cpp
@@ -1,16 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include "RHMap.h"
 
-// Test case to verify initialization of Robin_Hood_HashMap
-TEST_CASE("Initialization of Robin_Hood_HashMap", "[HashMap]") {
-    RHMap<int, std::vector<int>> *map = new RHMap<int, std::vector<int>>(10ll);
-    map->emplace(1, std::vector<int>{2, 3});
-    map->emplace(2, std::vector<int>{4, 5});
-    map->emplace(3, std::vector<int>{6, 7});
-    REQUIRE(map->get_count() == 3);
-    REQUIRE(map->get_capacity() == 1 << (sizeof(size_t) * 8 - __builtin_clzll(10 - 1)));
-    delete map;
-}
 //Test case to check if end() returns casted NULL pointer
 TEST_CASE("End Function of Robin_Hood_HashMap", "[HashMap]") {
     RHMap<int, std::vector<int>> map(10ll);
